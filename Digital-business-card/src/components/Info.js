f73763495885f3b5c6fde326ac74b02f3a91270b
acindms <acindms@gmail.com>
2024-11-20 02:11:28 +0800

    upgrade

---
+++ Digital-business-card/src/components/Info.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
+export default function Info() {
+    return (
+        <div className="info">
+            <img className="photo" src="./src/assets/img/img1.png" alt="picture"/>
+            <h1>Franklin X</h1>
+            <h2>Frontend Developer</h2>
+            <a target="_blank" href="acindm.github.io"><p>acindm.github.io</p></a>
+            <div className="buttons">
+                <button className="button-email">
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    Email
+                    </button>
+                <button className="button-linkedin">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                    LinkedIn
+                    </button>
+            </div>
+        </div>
+    )
+  }
