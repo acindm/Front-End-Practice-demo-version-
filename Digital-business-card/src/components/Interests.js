bdb333ec709b44a8a1d2207faa725ab4988b4014
acindms <acindms@gmail.com>
2024-11-20 16:53:34 +0800

    update

--- Digital-business-card/src/components/Interests.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-export default function Interests() {
-    return (
-      <section className="interests">
-        <h2>Interests</h2>
-          <p>Reader. Play Guqin. Sports-lover:running&trekking&swimming. Internet fanatic. white-hat hacker.</p>
-      </section>
-    )
-  }
