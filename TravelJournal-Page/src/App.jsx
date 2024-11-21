a84a0f386367487ceef716fa80cf3e0f51111d55
acindms <acindms@gmail.com>
2024-11-21 23:42:06 +0800

    TravelJournal update: Entry.jsx & props

--- TravelJournal-Page/src/App.jsx
+++ TravelJournal-Page/src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import Header from './assets/components/Header'
+import Entry from './assets/components/Entry'
+import data from './data'
 
 export default function App(){
+  const entryElements = data.map((entry) =>{
+    return (
+      <Entry 
+        img={entry.img}
+        title={entry.title}
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        dates={entry.dates}
+        text={entry.text}
+      />
+    )
+  })
   return(
+
     <div>
       <Header />
+      {entryElements}
     </div>
   )
 }
---
+++ TravelJournal-Page/src/assets/components/Entry.jsx
@@ -0,0 +1,30 @@
+export default function Entry(props){
+    return(
+        <div className="card">
+            <div>
+                <img className="main-img" 
+                src={props.img.src} 
+                alt={props.img.alt} />
+            </div>    
+            <div className="info-container">
+                <div className="location-container">
+                    <img 
+                    className="marker-icon" 
+                    src="./src/assets/images/marker.png" 
+                    alt="marker-icon" />
+                    <span>{props.country}</span>
+                    <a 
+                    target="_blank"
+                    href={props.googleMapsLink}>
+                    View On Google Map
+                    </a>
+               </div>
+                    <h2>{props.title}</h2>
+                <div className="text-container"> 
+                    <p className="trip-dates">{props.dates}</p>
+                    <p className="description">{props.text}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
