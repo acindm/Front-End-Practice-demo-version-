db70ce854b8e57f759d6720ffe285317eb8742f9
acindms <acindms@gmail.com>
2024-11-23 19:38:33 +0800

    Update Main.jsx

--- Claude-Of-Chef/src/assets/components/Main.jsx
+++ Claude-Of-Chef/src/assets/components/Main.jsx
@@ -1,13 +1,28 @@
 export default function Main() {
+    const ingredient = ["Chicken", "Oregano", "Tomato"]
+    const ingredientListItems = ingredient.map(ingredient =>(
+         <li key={ingredient}>{ingredient}</li>)
+    )
+    function handleSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredient.push(newIngredient)
+        
+    }
   return (
     <main>
-        <form className="add-ingredient-form">
+        <form onSubmit={handleSubmit} className="add-ingredient-form">
             <input
+                name="ingredient"
                 type="text"
                 placeholder="e.g. oregano"
                 aria-label="Add ingredient" />
-            <button type="submit">Add ingredient</button>
+            <button type="submit"> Add ingredient</button>
         </form>
+        <ul>
+            {ingredientListItems}
+        </ul>
     </main>
   )
 }
